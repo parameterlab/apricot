6013b56566b973d3aa4b0afc60abc0ca3f61b4aa
Martin Gubri <1850174+Framartin@users.noreply.github.com>
2024-11-19 18:09:43 -0600

    change model save name

--- run_regression_experiment.py
+++ run_regression_experiment.py
@@ -632,7 +632,7 @@ def run_single_calibration_experiment(
     # Model saving
     if model_save_dir is not None:
         model_type = "binary" if use_binary_targets else "clustering"
-        model_name = f"{calibration_model_identifier}_{model_type}_for_{model_identifier}_{dataset_name}"
+        model_name = f"apricot_{model_type}_{dataset_name}_{calibration_model_identifier.replace('/', '-')}_for_{model_identifier.replace('/', '-')}"
         model_save_path = os.path.join(model_save_dir, model_name)
 
         if not os.path.exists(model_save_path):
